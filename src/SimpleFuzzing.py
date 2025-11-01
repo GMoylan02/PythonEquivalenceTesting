e989d5ec0f3d8e477486ee26556ecad9e13177fe
GMoylan02 <gerardmoylan02@gmail.com>
2025-11-01 17:16:17 +0000

    Add simple example for Hypothesis and fuzz testing

---
+++ src/SimpleFuzzing.py
@@ -0,0 +1,23 @@
+from hypothesis import given, strategies as st
+
+def sum_1(nums):
+    total = 0
+    for n in nums:
+        total += n
+    return total
+
+
+def sum_2(nums):
+    total = 0
+    for n in nums:
+        # Weird edge case to test fuzzing
+        if n == 54:
+            total += 1
+        else:
+            total += n
+    return total
+
+
+@given(st.lists(st.integers(), max_size=54))
+def test_sum_equivalence(nums):
+    assert sum_1(nums) == sum_2(nums)
