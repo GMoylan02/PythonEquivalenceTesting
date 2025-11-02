17f39504183a54e93d0a4babad961648a07e8262
GMoylan02 <gerardmoylan02@gmail.com>
2025-11-02 16:03:50 +0000

    Add comment explaining findings

--- src/SimpleFuzzing.py
+++ src/SimpleFuzzing.py
@@ -20,4 +20,12 @@ def sum_2(nums):
 
 @given(st.lists(st.integers(), max_size=54))
 def test_sum_equivalence(nums):
+    """
+    Generally, for max_size < 54 this fails to catch the difference when running Hypothesis alone.
+    If you start a run at max_size >= 54, get hypothesis to catch that the functions are different, and then change
+    max_size to a lower value and re-run, Hypothesis will be able to catch that the functions are different likely due
+    to caching in the .hypothesis folder.
+
+    HypoFuzz always catches that the functions are different quite fast, no matter the value of max_size
+    """
     assert sum_1(nums) == sum_2(nums)
